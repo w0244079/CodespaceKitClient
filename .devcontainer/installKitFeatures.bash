0268a1f7abcf2ddfbeecb05caf4c448e0cdf4913
w0244079 <michael.caines@nscc.ca>
2025-01-09 09:47:54 -0400

    Update installKitFeatures.bash

--- .devcontainer/installKitFeatures.bash
+++ .devcontainer/installKitFeatures.bash
@@ -2,7 +2,10 @@
 
 # If the folder that was opened is a kit (.kit exists) install the kit features.
 if [ -d .kit ] && [ "$(pwd)" != "/home/$USER" ]; then
+  echo ".kit folder found"
   .kit/install-features-into-client/run.sh
+else
+  echo ".kit folder not found"
 fi
 
 # Run a new shell to pickup the changes.
